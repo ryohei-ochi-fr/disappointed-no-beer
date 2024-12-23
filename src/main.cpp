d425bedab2d46b7cc2ebc2c7cc63f49e13de1c3e
ryohei-ochi-fr <ryohei.ochi@futurerays.biz>
2024-12-24 08:30:54 +0900

    微修正

--- src/main.cpp
+++ src/main.cpp
@@ -12,16 +12,20 @@
 // #define LED_PIN 15
 // #define HALF_LOOP 500
 
-const char *ssid="Archer-C6-2G";
-const char *password="1234567890";
+const char *ssid = "Archer-C6-2G";
+const char *password = "1234567890";
 WiFiClient espClient;
 
 // #define MQTT_LUXLED "LuxLED"
 #define MQTT_BEERS "my/beers"
 const char *mqtt_server = "toad.rmq.cloudamqp.com";
-const char* mqtt_user = "uoqpcaih:uoqpcaih";
-const char* mqtt_password = "1VxyoFDD97p5zd9EWGGa0qBt6mjTZIAt";
+const char *mqtt_user = "uoqpcaih:uoqpcaih";
+const char *mqtt_password = "1VxyoFDD97p5zd9EWGGa0qBt6mjTZIAt";
 PubSubClient client(espClient);
+
+// 定数の定義
+const int JSON_DOC_SIZE = 200;
+const int SLEEP_DURATION_MS = 1800000; // 30分
 
 #define DHTPIN 15
 #define DHTTYPE DHT11
@@ -31,8 +35,8 @@ HX711 scale;
 const int DT_PIN = 18;
 const int SCK_PIN = 19;
 
-const int ADIN_1 = 33;  // ADC1
-const int VDD = 3.3; // 電源 3.3V
+const int ADIN_1 = 33;       // ADC1
+const int VDD = 3.3;         // 電源 3.3V
 const int ANALOG_MAX = 4096; // 12bit = 4096
 
 long lastMsg = 0;
@@ -41,25 +45,31 @@ int value = 0;
 
 // https://github.com/bogde/HX711/tree/master
 
-void callback(char* topic, byte* message, unsigned int length) {
+void callback(char *topic, byte *message, unsigned int length)
+{
   Serial.print("Message arrived [");
   Serial.print(topic);
   Serial.print("] ");
-  for (int i = 0; i < length; i++) {
+  for (int i = 0; i < length; i++)
+  {
     Serial.print((char)message[i]);
   }
   Serial.println();
 }
 
-
-void reconnect() {
-  while (!client.connected()) {
+void reconnect()
+{
+  while (!client.connected())
+  {
     Serial.print("Attempting MQTT connection...");
     // client.connect の第2引数と第3引数にユーザー名とパスワードを追加
-    if (client.connect("ESP32Client", mqtt_user, mqtt_password)) {
+    if (client.connect("ESP32Client", mqtt_user, mqtt_password))
+    {
       Serial.println("connected");
       client.subscribe(MQTT_BEERS);
-    } else {
+    }
+    else
+    {
       Serial.print("failed, rc=");
       Serial.print(client.state());
       Serial.println(" try again in 5 seconds");
@@ -68,13 +78,12 @@ void reconnect() {
   }
 }
 
-
-
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
   Serial.begin(115200);
   delay(100);
-  Serial.printf("%s - run\n",__func__);
+  Serial.printf("%s - run\n", __func__);
   // pinMode(LED_PIN, OUTPUT);
 
   // pinMode(ADIN_1, ANALOG);
@@ -85,7 +94,8 @@ void setup() {
   dht.begin();
 
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.println("connecting");
   }
@@ -93,17 +103,20 @@ void setup() {
 
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
-
 }
 
-
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
   // digitalWrite(LED_PIN, HIGH);
   // Serial.printf("%s - LED_PIN - HIGH\n",__func__);
   // delay(HALF_LOOP);
- 
-  long value = scale.read_average(5) * -1;
+
+  long value = scale.read_average(5);
+  if (value < 0)
+  {
+    value = 0;
+  }
   Serial.print("scale:  ");
   Serial.print(value);
   Serial.println("mV");
@@ -115,33 +128,29 @@ void loop() {
   // digitalWrite(LED_PIN, LOW);
   // Serial.printf("%s - LED_PIN - LOW\n",__func__);
   // delay(HALF_LOOP);
- 
+
   // int ADC = analogRead(ADIN_1);
   // Serial.print("Llight: ");
   // Serial.print(ADC);
   // Serial.println("mV");
 
-  if (!client.connected()) {
+  if (!client.connected())
+  {
     reconnect();
   }
   client.loop();
 
-  // char buffer[10]; // 変換後の文字列を格納するための配列 
+  // char buffer[10]; // 変換後の文字列を格納するための配列
   // itoa(ADC, buffer, 10); // 10は基数（decimal）
   // itoa(value, buffer, 10); // 10は基数（decimal）
 
   // JSONドキュメントの作成
-  StaticJsonDocument<200> doc;
-  // DynamicJsonDocument doc(200);
+  StaticJsonDocument<JSON_DOC_SIZE> doc;
   doc["scale"] = String(value);
   doc["temperature"] = String(t, 1);
   doc["humidity"] = String(h, 0);
 
-  // doc["scale"] = value;
-  // doc["temperature"] = t;
-  // doc["humidity"] = h;
-
-  char buffer[200];
+  char buffer[JSON_DOC_SIZE];
   serializeJson(doc, buffer);
 
   Serial.print("Publishing message: ");
@@ -150,8 +159,8 @@ void loop() {
   // MQTTにパブリッシュ
   // client.publish(MQTT_BEERS, buffer);
   client.publish("my/beers/scale", String(value).c_str());
-  client.publish("my/beers/temperature", String(t,1).c_str());
-  client.publish("my/beers/humidity", String(h,0).c_str());
+  client.publish("my/beers/temperature", String(t, 1).c_str());
+  client.publish("my/beers/humidity", String(h, 0).c_str());
 
   // 30秒 非同期パブリッシュの待機
   // delay(30000);
@@ -166,5 +175,4 @@ void loop() {
 
   // 30分
   delay(300000);
-
 }
